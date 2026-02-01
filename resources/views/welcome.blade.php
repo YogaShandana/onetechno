ff9c06d5de36f946359f1a2d0fc51166c7a567e7
YogaShandana <164825120+YogaShandana@users.noreply.github.com>
2026-02-01 20:46:50 +0800

    Remove Vite dependency, use static assets directly for hosting compatibility

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,11 +7,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>one techno porto</title>
     <link rel="icon" type="image/png" href="{{ asset('img/tab.png') }}">
-    @if(app()->environment('production'))
-        <link rel="stylesheet" href="{{ asset('build/assets/app-BC1mE-tT.css') }}">
-    @else
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    @endif
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body class="bg-white font-sans antialiased overflow-hidden selection:bg-yellow-400 selection:text-black">
     <div class="scroll-container">
@@ -24,8 +20,6 @@
     @include('faq')
     @include('testimoni')
 
-    @if(app()->environment('production'))
-        <script src="{{ asset('build/assets/app-B7xPJ507.js') }}"></script>
-    @endif
+    <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
